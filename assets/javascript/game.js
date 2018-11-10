4dad8815894787fa8d8e83e1d2fb15b3fecf7f19
AChapman <achapman77@gmail.com>
2018-11-10 13:58:44 -0700

    Successfully refactored resets and display, could not figure out how to refactor validateKeystroke and psychicAbility

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
 //Create array of choices for computer
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u" ,"v", "w", "x", "y", "z"]
 
+//Initialize Randomized computer guess 
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log("This is the initialize computerChoice: " + computerChoice)
+
 //Create blank array to hold user guesses for future display
 var userGuessesArray = [];
 
-//Create variables to hold number of wins, losses
+//Create variables to hold number of wins, losses, sessions
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
@@ -22,77 +26,90 @@ var evaluationText = document.getElementById("evaluation-text");
 var evaluationText2 = document.getElementById("evaluation-text2");
 
 //Message to instructors
-// alert("Welcome.  I have console.logged the computer choices if you would like to cheat the system.  I have also added keystroke validation to exclude duplicates and anything other than alphabet.  There are also escalating psychic evaluation messages.");
+alert("Welcome.  I have console.logged the computer choices if you would like to cheat the system.");
+
+function resetPage() {    
+    evaluation = 0;
+    wins = 0;
+    losses = 0;
+    sessionsLeft = 10;
+    resetSession();
+}
+
+function resetSession() {
+    userGuessesArray = [];
+    guessesLeft = 10;
+    //Randomized computer guess 
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log("This is the reset computerChoice: " + computerChoice)
+};
+
+function display (){
+    //Display the user guesses and wins/losses/ties
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft;
+    sessionsLeftText.textContent = sessionsLeft;
+    userGuessesText.textContent = userGuessesArray.toString();
+}
 
 
-//Initialize Randomized computer guess 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log("This is the initialize computerChoice: " + computerChoice)
+
 
 
 //Evaluate user guess by key event
 document.onkeyup = function(event) {
-    function resetPage() {    
-        evaluation = 0;
-        wins = 0;
-        losses = 0;
-        sessionsLeft = 10;
-        resetSession();
-    }
     
-    function resetSession() {
-        userGuessesArray = [];
-        guessesLeft = 10;
-        //Randomized computer guess 
-        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-        console.log("This is the reset computerChoice: " + computerChoice)
-    };
-    
+
     //Determines what key was pressed by user
     var userGuess = event.key.toLowerCase();
     // console.log(userGuess); // TEST OK
     
+    // ********Tried to refactor this into function outside of onkeyup but couldn't get it to work.  Don't really understand why.********
     //Validate user keystroke 
-    function validateUserKey() {
-        if (userGuessesArray.includes(userGuess)) {
-        //Excludes entry of duplicate letters in array.  
-        //Maintains # of guessesLeft    
-        alert("You have already entered this letter genius.");
-        guessesLeft++;
-        } else if (alphabet.includes(userGuess)){
-        //Add user guess to userGuessesArray
-        userGuessesArray.push(userGuess);
-        // console.log(userGuessesArray); //TEST OK
-        } else {
-        alert("Not a valid letter of the alphabet.");
-        guessesLeft++;
-        }
+    //Excludes entry of duplicate letters in array.
+    if (userGuessesArray.includes(userGuess)) {  
+    alert("You have already entered this letter genius.");
+    //Maintains # of guessesLeft   
+    guessesLeft++;
+    } else if (alphabet.includes(userGuess)){
+    //Add user guess to userGuessesArray
+    userGuessesArray.push(userGuess);
+    // console.log(userGuessesArray); //TEST OK
+    } else {
+    alert("Not a valid letter of the alphabet.");
+    guessesLeft++;
     };
     
 
-
-    //Evaluate user vs. compter 
+    //Evaluate userGuess vs. compterChoice 
     if (sessionsLeft === 0) {
         resetPage();
-        alert("Thank you for your participation.  You can now put your tinfoil hat back on.");
+        alert("Thank you for your participation.  You can now put your tinfoil hat back on.")
+        display();
     } else if (guessesLeft <= 1) {
         losses++;
+        sessionsLeft--;
         resetSession();
+        display();
        
     } else if (userGuess === computerChoice) {
         wins++;
         sessionsLeft--;
         resetSession();
+        display();
     } else {
          //Subtract 1 from guessesLeft
-         validateUserKey();
          guessesLeft--;
+         display();
     };
 
     
-    
+    // ********Tried to refactor this into function outside of onkeyup but couldn't get it to work.  Don't really understand why.********
+
     //Evaluate user psychic ability
     var evaluation = (wins/10)*100;
+
     if (evaluation <= 30) {
         evaluationText.textContent = evaluation.toFixed(2) + "% psychic ability.";
         evaluationText2.textContent = "Even a monkey is better than this.";
@@ -103,14 +120,12 @@ document.onkeyup = function(event) {
     } else {
         evaluationText.textContent = evaluation.toFixed(2) + "% psychic ability. ";
         evaluationText2.textContent = "An NSA operative will contact you shortly.";
-    }
+    };
 
-    //Display the user guesses and wins/losses/ties
-    winsText.textContent = wins;
-    lossesText.textContent = losses;
-    guessesLeftText.textContent = guessesLeft;
-    sessionsLeftText.textContent = sessionsLeft;
-    userGuessesText.textContent = userGuessesArray.toString();
+    
+
+
+    
 }
 
 
